ec4bcd2793bbdbb14eadaa562d72b02d2d988c7e
Scott Briggs <briggsscott12@gmail.com>
2025-09-21 15:00:50 +0100

    more

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -2,7 +2,7 @@ export default function AnnouncementBar(){
   return (
     <div className="w-full h-10 bg-black text-white text-sm font-medium
                     flex items-center justify-center fixed top-0 left-0 z-[60] shadow-lg">
-      <span className="text-sm">✨ Get 2 GLINTS + Free Worldwide Shipping for £29.99</span>
+      <span className="text-sm">Get 2 serums for £29.99 + free worldwide shipping</span>
     </div>
   );
 }
